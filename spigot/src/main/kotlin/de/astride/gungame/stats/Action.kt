d714ad683cc5439fb08ba0f78151e33d1ffc1c11
LartyHD <lartyhd@gmail.com>
2019-04-01 15:26:14 +0200

    Add argument verification to Action

--- spigot/src/main/kotlin/de/astride/gungame/stats/Action.kt
+++ spigot/src/main/kotlin/de/astride/gungame/stats/Action.kt
@@ -3,10 +3,17 @@ package de.astride.gungame.stats
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 30.03.2019 17:14.
- * Current Version: 1.0 (30.03.2019 - 30.03.2019)
+ * Current Version: 1.0 (30.03.2019 - 01.04.2019)
  */
 data class Action(
     val id: String,
     val meta: Map<String, Any?> = mapOf(),
     val timestamp: Long = System.currentTimeMillis()
-)
+) {
+
+    init {
+        if ("id" in meta) throw IllegalArgumentException("\"id\" must not be present in \"meta\"")
+        if ("timestamp" in meta) throw IllegalArgumentException("\"timestamp\" must not be present in \"meta\"")
+    }
+
+}
