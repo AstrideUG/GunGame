6a7230f18a5caa3e2a2e2bd484041f744930b61a
LartyHD <lartyhd@gmail.com>
2019-04-04 16:14:07 +0200

    Changed lastDamage & lastDamageCause to nullable in Damageable

--- spigot/src/main/kotlin/de/astride/data/Damageable.kt
+++ spigot/src/main/kotlin/de/astride/data/Damageable.kt
@@ -5,11 +5,11 @@ import org.bukkit.event.entity.EntityDamageEvent
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 02.04.2019 02:44.
- * Current Version: 1.0 (02.04.2019 - 02.04.2019)
+ * Current Version: 1.0 (02.04.2019 - 04.04.2019)
  */
 data class Damageable(
     val maximumNoDamageTicks: Int,
     val noDamageTicks: Int,
-    val lastDamage: Double,
-    val lastDamageCause: EntityDamageEvent
+    val lastDamage: Double?,
+    val lastDamageCause: EntityDamageEvent?
 )
