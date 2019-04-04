3947a495f4c28f1b7a2bbd97ea866c5e78a3cc47
LartyHD <lartyhd@gmail.com>
2019-04-04 17:16:13 +0200

    Changed spectatorTarget type to nullable

--- spigot/src/main/kotlin/de/astride/data/Targetable.kt
+++ spigot/src/main/kotlin/de/astride/data/Targetable.kt
@@ -1,13 +1,14 @@
 package de.astride.data
 
 import org.bukkit.Location
+import org.bukkit.entity.Entity
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 02.04.2019 02:04.
- * Current Version: 1.0 (02.04.2019 - 02.04.2019)
+ * Current Version: 1.0 (02.04.2019 - 04.04.2019)
  */
 data class Targetable(
-    val spectatorTarget: org.bukkit.entity.Entity,
+    val spectatorTarget: Entity?,
     val compassTarget: Location
 )
