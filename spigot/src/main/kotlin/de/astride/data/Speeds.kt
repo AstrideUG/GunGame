1dad81bc6339152c5c09e5d21ea2fcdf5304fc6e
LartyHD <lartyhd@gmail.com>
2019-04-05 04:50:24 +0200

    Added kotlinx.serialization annotations to Speeds

--- spigot/src/main/kotlin/de/astride/data/Speeds.kt
+++ spigot/src/main/kotlin/de/astride/data/Speeds.kt
@@ -1,10 +1,13 @@
 package de.astride.data
 
+import kotlinx.serialization.Serializable
+
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 02.04.2019 02:06.
- * Current Version: 1.0 (02.04.2019 - 02.04.2019)
+ * Current Version: 1.0 (02.04.2019 - 04.04.2019)
  */
+@Serializable
 data class Speeds(
     val isSprinting: Boolean,
     val isSneaking: Boolean,
