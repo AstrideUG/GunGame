beaa75664289fbe5c83e886c920f33b93ae824f4
LartyHD <lartyhd@gmail.com>
2019-04-05 05:36:59 +0200

    Added kotlinx.serialization annotations to Action

--- spigot/src/main/kotlin/de/astride/gungame/stats/Action.kt
+++ spigot/src/main/kotlin/de/astride/gungame/stats/Action.kt
@@ -1,10 +1,13 @@
 package de.astride.gungame.stats
 
+import kotlinx.serialization.Serializable
+
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 30.03.2019 17:14.
- * Current Version: 1.0 (30.03.2019 - 01.04.2019)
+ * Current Version: 1.0 (30.03.2019 - 04.04.2019)
  */
+@Serializable
 data class Action(
     val id: String,
     val meta: Map<String, Any?> = mapOf(),
