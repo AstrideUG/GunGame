4bc416766ffec046491f4cd5ee4a76fb54eab8c6
LartyHD <lartyhd@gmail.com>
2019-04-05 04:47:37 +0200

    Added kotlinx.serialization annotations to Inventories

--- spigot/src/main/kotlin/de/astride/data/Inventories.kt
+++ spigot/src/main/kotlin/de/astride/data/Inventories.kt
@@ -1,18 +1,20 @@
 package de.astride.data
 
+import kotlinx.serialization.Serializable
 import org.bukkit.inventory.*
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 02.04.2019 02:14.
- * Current Version: 1.0 (02.04.2019 - 02.04.2019)
+ * Current Version: 1.0 (02.04.2019 - 04.04.2019)
  */
+@Serializable
 data class Inventories(
     val canPickupItems: Boolean,
-    val equipment: EntityEquipment,
-    val enderChest: Inventory,
-    val openInventory: InventoryView?,
-    val inventory: PlayerInventory,
-    val itemInHand: ItemStack?,
-    val itemOnCursor: ItemStack?
+    @Serializable(with = EntityEquipmentSerializer::class) val equipment: EntityEquipment,
+    @Serializable(with = InventorySerializer::class) val enderChest: Inventory,
+    @Serializable(with = InventoryViewSerializer::class) val openInventory: InventoryView?,
+    @Serializable(with = InventorySerializer::class) val inventory: PlayerInventory,
+    @Serializable(with = ItemStackSerializer::class) val itemInHand: ItemStack?,
+    @Serializable(with = ItemStackSerializer::class) val itemOnCursor: ItemStack?
 )
