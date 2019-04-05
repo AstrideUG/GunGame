1db2b1d031b34f81447bf872c44f3302b7a4601f
LartyHD <lartyhd@gmail.com>
2019-04-05 04:57:25 +0200

    Added an de- and serialize test for DataExp

---
+++ spigot/src/test/kotlin/de/astride/data/DataExpTest.kt
@@ -0,0 +1,22 @@
+package de.astride.data
+
+import org.junit.Test
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 05.04.2019 01:38.
+ * Current Version: 1.0 (05.04.2019 - 05.04.2019)
+ */
+class DataExpTest {
+
+    //given
+    companion object {
+        const val expectedJson: String =
+            """{"level":0,"exp":0.0,"exhaustion":0.0,"expToLevel":0,"totalExperience":0}"""
+        val expectedSource: DataExp = DataExp(0, 0f, 0f, 0, 0)
+    }
+
+    @Test
+    fun `(de)serialize`(): Unit = `(de)serialize`(expectedJson, expectedSource)
+
+}
