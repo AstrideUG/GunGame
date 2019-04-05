5dc17e56862dda433a2b46485528835a77f4569a
LartyHD <lartyhd@gmail.com>
2019-04-05 04:50:03 +0200

    Added kotlinx.serialization annotations to Permissables

--- spigot/src/main/kotlin/de/astride/data/Permissables.kt
+++ spigot/src/main/kotlin/de/astride/data/Permissables.kt
@@ -1,13 +1,15 @@
 package de.astride.data
 
+import kotlinx.serialization.Serializable
 import org.bukkit.permissions.PermissionAttachmentInfo
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 02.04.2019 02:01.
- * Current Version: 1.0 (02.04.2019 - 02.04.2019)
+ * Current Version: 1.0 (02.04.2019 - 05.04.2019)
  */
+@Serializable
 data class Permissables(
     val isOp: Boolean,
-    val effectivePermissions: Set<PermissionAttachmentInfo>
+    @Serializable(with = PermissionAttachmentInfoSerializer::class) val effectivePermissions: Set<PermissionAttachmentInfo>
 )
