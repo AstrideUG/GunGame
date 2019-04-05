a478b5b899ffac6b267bc57f7fa7aedbb6ae02cc
LartyHD <lartyhd@gmail.com>
2019-04-05 04:47:57 +0200

    Added kotlinx.serialization annotations to MetaData

--- spigot/src/main/kotlin/de/astride/data/MetaData.kt
+++ spigot/src/main/kotlin/de/astride/data/MetaData.kt
@@ -1,17 +1,19 @@
 package de.astride.data
 
+import kotlinx.serialization.Serializable
 import java.net.InetSocketAddress
 import java.util.*
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 02.04.2019 02:18.
- * Current Version: 1.0 (02.04.2019 - 02.04.2019)
+ * Current Version: 1.0 (02.04.2019 - 04.04.2019)
  */
+@Serializable
 data class MetaData(
-    val address: InetSocketAddress,
-    val rawAddress: InetSocketAddress,
-    val uniqueId: UUID,
+    @Serializable(with = InetSocketAddressSerializer::class) val address: InetSocketAddress,
+    @Serializable(with = InetSocketAddressSerializer::class) val rawAddress: InetSocketAddress,
+    @Serializable(with = UUIDSerializer::class) val uniqueId: UUID,
     val hasPlayedBefore: Boolean,
     val lastPlayed: Long,
     val firstPlayed: Long,
