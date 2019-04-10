56e128f74585059eeb72cb8b2008a9a22ee62948
LartyHD <lartyhd@gmail.com>
2019-04-10 09:28:18 +0200

    Added a comment how Bukkit is dealing with the Exp stuff

--- spigot/src/main/kotlin/de/astride/data/DataExp.kt
+++ spigot/src/main/kotlin/de/astride/data/DataExp.kt
@@ -6,6 +6,12 @@ import kotlinx.serialization.Serializable
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 02.04.2019 01:54.
  * Current Version: 1.0 (02.04.2019 - 10.04.2019)
+ *
+ * Bukkit:
+ * level = expLevel
+ * exp = exp
+ * expToLevel = this.expLevel >= 30 ? 112 + (this.expLevel - 30) * 9 : (this.expLevel >= 15 ? 37 + (this.expLevel - 15) * 5 : 7 + this.expLevel * 2);
+ * totalExperience = expTotal
  */
 @Serializable
 data class DataExp(
