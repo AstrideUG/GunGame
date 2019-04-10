97eab4e3cd2f5796bb86af2fc99700c1b9147603
LartyHD <lartyhd@gmail.com>
2019-04-11 00:42:38 +0200

    Deleted "exhaustion" in DataExpTest

--- spigot/src/test/kotlin/de/astride/data/DataExpTest.kt
+++ spigot/src/test/kotlin/de/astride/data/DataExpTest.kt
@@ -5,15 +5,15 @@ import org.junit.Test
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 05.04.2019 01:38.
- * Current Version: 1.0 (05.04.2019 - 05.04.2019)
+ * Current Version: 1.0 (05.04.2019 - 11.04.2019)
  */
 class DataExpTest {
 
     //given
     companion object {
         const val expectedJson: String =
-            """{"level":0,"exp":0.0,"exhaustion":0.0,"expToLevel":0,"totalExperience":0}"""
-        val expectedSource: DataExp = DataExp(0, 0f, 0f, 0, 0)
+            """{"level":0,"exp":0.0,"expToLevel":0,"totalExperience":0}"""
+        val expectedSource: DataExp = DataExp(0, 0f, 0, 0)
     }
 
     @Test
