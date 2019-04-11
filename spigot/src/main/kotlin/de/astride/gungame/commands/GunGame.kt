bc585870bee2b7887876a1314e26d464daee3b71
LartyHD <lartyhd@gmail.com>
2019-04-11 02:21:44 +0200

    Added directory support to "GunGame save actions" Command

--- spigot/src/main/kotlin/de/astride/gungame/commands/GunGame.kt
+++ spigot/src/main/kotlin/de/astride/gungame/commands/GunGame.kt
@@ -9,17 +9,18 @@ import net.darkdevelopers.darkbedrock.darkness.spigot.commands.Command
 import net.darkdevelopers.darkbedrock.darkness.spigot.functions.sendTo
 import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 04.04.2019 18:33.
- * Current Version: 1.0 (04.04.2019 - 06.04.2019)
+ * Current Version: 1.0 (04.04.2019 - 11.04.2019)
  */
 class GunGame(javaPlugin: JavaPlugin) : Command(
     javaPlugin,
     commandName = config.name,
     permission = config.permission,
-    usage = "save actions [File]",
+    usage = "save actions [Path]",
     minLength = 2,
     maxLength = 3,
     aliases = *config.aliases
@@ -27,8 +28,11 @@ class GunGame(javaPlugin: JavaPlugin) : Command(
 
     override fun perform(sender: CommandSender, args: Array<String>) {
 
-        val configData =
-            if (args.size < 3) configService.actions.configData else ConfigData(javaPlugin.dataFolder, args[2])
+        val configData = if (args.size < 3) configService.actions.configData else {
+            val path = args[2].split('/')
+            val directory = "${javaPlugin.dataFolder}${File.separator}${path.dropLast(1).joinToString(File.separator)}"
+            ConfigData(directory, path.last())
+        }
         configService.actions.save(allActions, configData)
 
         val path = configData.file.toPath()
