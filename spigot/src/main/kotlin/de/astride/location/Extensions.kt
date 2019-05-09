f5621521753f0db5cdaf912a35baba7b60cd7dba
LartyHD <lartyhd@gmail.com>
2019-05-09 13:36:33 +0200

    Added  org.bukkit.Location.toLocation(): Location  Map<String, Any>.toLocation(): Location

--- spigot/src/main/kotlin/de/astride/location/Extensions.kt
+++ spigot/src/main/kotlin/de/astride/location/Extensions.kt
@@ -1,5 +1,6 @@
 package de.astride.location
 
+import de.astride.location.data.DataLocation
 import org.bukkit.Bukkit
 
 /**
@@ -14,4 +15,15 @@ fun Location.toBukkitLocation(): org.bukkit.Location = org.bukkit.Location(
     vector.z,
     lookable?.yaw ?: 0f,
     lookable?.pitch ?: 0f
+)
+
+fun org.bukkit.Location.toLocation(): Location = DataLocation(world?.name ?: "null", x, y, z, yaw, pitch)
+
+fun Map<String, Any>.toLocation(): Location = DataLocation(
+    this["world"] as? String ?: "null",
+    this["x"] as? Double ?: 0.0,
+    this["y"] as? Double ?: 0.0,
+    this["z"] as? Double ?: 0.0,
+    this["yaw"] as? Float ?: 0f,
+    this["pitch"] as? Float ?: 0f
 )
