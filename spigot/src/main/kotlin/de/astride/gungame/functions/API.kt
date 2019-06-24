ac0025bfdb8dbaf1dd043259f3f4977a83ddebb0
LartyHD <lartyhd@gmail.com>
2019-06-24 09:30:42 +0200

    Deleted a unused function

--- spigot/src/main/kotlin/de/astride/gungame/functions/API.kt
+++ spigot/src/main/kotlin/de/astride/gungame/functions/API.kt
@@ -1,8 +1,5 @@
 package de.astride.gungame.functions
 
-import org.bukkit.Location
-import org.bukkit.World
-
 /*
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 06.04.2019 04:08.
@@ -23,27 +20,3 @@ fun String?.replace(key: String, value: Any): String? = this?.replace(key, value
  * Current Version: 1.0 (06.04.2019 - 06.04.2019)
  */
 fun String.replace(key: String, value: Any): String = replace("@$key@", value.toString(), ignoreCase = true)
-
-/**
- * @author Lars Artmann | LartyHD
- * Created by Lars Artmann | LartyHD on 06.05.2019 08:30.
- *
- * TODO: move to Darkness
- *
- * Current Version: 1.0 (06.05.2019 - 06.05.2019)
- */
-fun Location.copy(
-    world: World? = null,
-    x: Double? = null,
-    y: Double? = null,
-    z: Double? = null,
-    yaw: Float? = null,
-    pitch: Float? = null
-): Location = Location(
-    world ?: this.world,
-    x ?: this.z,
-    y ?: this.y,
-    z ?: this.z,
-    yaw ?: this.yaw,
-    pitch ?: this.pitch
-)
